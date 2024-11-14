722d8e89797c3faadbc9c2a16293d16e8522ac42
Elijah Rogers <ejrogersemail@gmail.com>
2024-11-14 12:09:46 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿
+
+namespace Challenge2_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Write a C# Sharp program to read roll no, name and marks of three subjects and calculate the total, percentage and division.
+            Console.Write("Enter Student Roll Number: ");
+            string rollNum = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Enter Student Name: ");
+            string stuName = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Enter Physics Grade %: ");
+            string phyGrade = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Enter Chemistry Grade %: ");
+            string chemGrade = Console.ReadLine();
+            Console.WriteLine();
+            Console.Write("Enter Computer Application Grade %: ");
+            string compAppGrade = Console.ReadLine();
+            Console.WriteLine();
+
+            Student newStudent = new Student();
+            //String to hold grade info
+            string studentInfoString = $"{phyGrade},{chemGrade},{compAppGrade}";
+
+            // Adds student information to student class
+            newStudent.RollNum = rollNum;
+            newStudent.Name = stuName;
+            newStudent.Grades.Add(rollNum, studentInfoString);
+
+            // Calculates max marks and total marks of student by rollNum
+            double tempPercentageTotal = newStudent.CalculateGrade(newStudent.RollNum, newStudent.Grades);
+            Console.WriteLine($"Max Marks: { newStudent.GradeCount * 100}");
+            Console.WriteLine($"Student Total Marks: { tempPercentageTotal }");
+            Console.WriteLine();
+
+            // Calculates the students overall percentage and divsion they are in
+            string tempPrecentageOverall = newStudent.CalculateGradePercentage(tempPercentageTotal);
+            string tempDivision = newStudent.DivisionSelection(tempPercentageTotal);
+            Console.WriteLine($"Student Overall Percentage: { tempPrecentageOverall }%");
+            Console.WriteLine($"Student Division: { tempDivision }");
+
+
+
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace Challenge2_4
+{
+	public class Student
+	{
+		public string RollNum { get; set; }
+		public string Name { get; set; }
+		public string OverallPercentage { get; set; } // Saves the overall percentage of the student
+
+		// This property is used to count how many grades are being calculated.
+		// Used for divisionSelection Method
+		public int GradeCount { get; set; } = 0;
+
+		public double GradeSummation { get; set; } = 0; // Holds total grade
+
+		private readonly Dictionary<string,string> grades = new Dictionary<string, string>(); // Hold grade information
+
+		public Dictionary<string,string> Grades
+		{
+			get { return grades; }
+		}
+
+        public Student()
+		{
+		}
+
+		public double CalculateGrade(string rollNum, Dictionary<string,string> grades)
+		{
+
+			string[] temp = grades[rollNum].Split(',');
+
+			foreach (string grade in temp)
+			{
+				GradeSummation += Convert.ToDouble(grade);
+				GradeCount++;
+			}
+			return GradeSummation;
+        }
+
+		// Finds the division the student is in
+		public string DivisionSelection(double gradeSummation)
+		{
+			// Gets total percentage point based on how many grades are added
+			double totalGrade = 100 * GradeCount;
+
+			// totalGrade Divided by four to be used to create buckets for each division
+			double subtractDivision = totalGrade / 4;
+
+			if (gradeSummation >= 0 && gradeSummation <= (totalGrade - (subtractDivision * 3)))
+			{
+				return "4th";
+			}else if(gradeSummation > (totalGrade - (subtractDivision * 3)) && gradeSummation <= (totalGrade - (subtractDivision * 2)))
+			{
+                return "3rd";
+			}else if(gradeSummation > (totalGrade - (subtractDivision * 2)) && gradeSummation <= (totalGrade - (subtractDivision)))
+			{
+                return "2nd";
+			}
+			else
+			{
+                return "1st";
+			}
+		}
+
+		// Calculates student overall percentage
+		public string CalculateGradePercentage(double gradeSummation)
+		{
+			OverallPercentage = Math.Round((gradeSummation / (100 * GradeCount) * 100), 2).ToString("0.00");
+
+            return OverallPercentage;
+		}
+	}
+}
